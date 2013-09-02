813451712c7e6137904eaa3194af8dea97676291
Edward Thang <RandomClown@gmail.com>
2013-09-02 12:21:08 -0700

    Canvas

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,1 +1,13 @@
 ﻿
+var canvas, ctx;
+var mouse={ x: 0, y: 0 };
+
+
+window.onload=function() { // Makes sure the website is loaded before running code
+	canvas=document.getElementById('canvas');
+	ctx=canvas.getContext('2d');
+
+
+	setInterval(function() {
+	}, 1000/g_fpsEngine);
+};
