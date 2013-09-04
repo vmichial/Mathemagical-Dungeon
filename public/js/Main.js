07ea9d9766995b7450819b08c7fdffd973be970b
Edward Thang <RandomClown@gmail.com>
2013-09-04 14:20:05 -0700

    Check out the sample

--- public/js/Main.js
+++ public/js/Main.js
@@ -2,7 +2,10 @@
 var canvas, ctx;
 var mouse={ x: 0, y: 0 };
 
+var r_board;
 var cat;
+
+var g_boardAngle=(Math.PI*0.152).toFixed(4);
 
 
 $(document).ready(function() {
@@ -10,11 +13,37 @@ $(document).ready(function() {
 	ctx=canvas.getContext('2d');
 
 
+	// Make a new game board
+
+	r_board=$('#board'); // Point to the game board
+	r_board.css('transform', 'rotate('+(g_boardAngle)+'rad)');
+	var board='', x=6, y=8, z=1, i;
+
+	for(j=0; j<x; ++j) {
+		var r_row=$('<div>', {
+			class: 'tileRow'
+		}).appendTo(r_board);
+		r_row.css('padding-left', (x-j)*32);
+
+		for(var k=0; k<y; ++k) {
+			var r_tile=$('<div>', {
+				class: 'tile t_greyF'
+			}).appendTo(r_row);
+			r_tile.css('margin-right', -44);
+			r_tile.css('margin-bottom', -60);
+			r_tile.css('transform', 'rotate('+(-g_boardAngle)+'rad) translate3d( 0px, 0px, 0px)');
+		}
+	}
+	//.on('click',function(){if($(this).
+
+
+
 	cat=new Ent('ent_nyancat');
 	cat.addTexture('nyanCat');
 	cat.addTexture('nyanCat', { x: 64, y: 64 });
 
 	cat._pos={ x: 100, y: 100 };
+
 
 	setInterval(function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
