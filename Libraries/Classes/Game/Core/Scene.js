2acc7c5a101807302b29d342fe9810d0237e7e1c
Michial Andrew Green II <vmichial@gmail.com>
2013-09-04 20:39:18 -0700

    backbone uploaded

--- Libraries/Classes/Game/Core/Scene.js
+++ Libraries/Classes/Game/Core/Scene.js
@@ -12,7 +12,7 @@ isActive = true;
 
 objectImage = new Image();
 
-The start function is called by init
+//The start function is called by init
 Start();
 }
 
@@ -25,7 +25,7 @@ Level(TileString){
 TileArray = new Array();
 }
 
-Scene(Type, PlayerData, coreSoundsArray, coreImagesArray, coreObjectsArray){
+Scene(Type, PlayerData, coreSoundsArray, coreImagesArray, coreObjectsArray, Parent){
     //defines the type of scene, a string with either, END,CUT,LEVEL,MENU
     type =Type;
     
@@ -38,12 +38,12 @@ Scene(Type, PlayerData, coreSoundsArray, coreImagesArray, coreObjectsArray){
     //The cutscene obeject,check the structures for more info
     Cinematic = new CutScene();
     
-//core objects represent an array of all objects that come prebuilt into the game
-//this includes blocks, traps, switches, trees, etc which are assigned to a tile
-coreObjects = coreObjectsArray;
+	//core objects represent an array of all objects that come prebuilt into the game
+	//this includes blocks, traps, switches, trees, etc which are assigned to a tile
+	coreObjects = coreObjectsArray;
 
-//custom objects are whatever objects you create for this specific scene.
-customObjects = new Array();
+	//custom objects are whatever objects you create for this specific scene.
+	customObjects = new Array();
 
     //coreSounds hold references to all of the core sounds everyone can have
     coreSounds = coreSoundsArray;
@@ -59,16 +59,15 @@ customObjects = new Array();
     //custom images should be initialized by the creator of the scene
     customImages = newArray();
     
-//initialize function will call the init of the correct type, and give a loading screen
-//while you wait for assets to load
-initilize();
-//set custom images lets you set the sources of each of your custom images causing the //page to start loading them, this function is called by init.
-setCustomImages();
+	//initialize function will call the init of the correct type, and give a loading screen
+	//while you wait for assets to load
+	initilize();
+	//set custom images lets you set the sources of each of your custom images causing the //page to start loading them, this function is called by init.
+	setCustomImages();
 
-//set custom sounds lets you initialize the custom sounds array for the sounds your 
-//scene needs, called by init
-setCustomSounds();
-
+	//set custom sounds lets you initialize the custom sounds array for the sounds your 
+	//scene needs, called by init
+	setCustomSounds();
 
 
 }
