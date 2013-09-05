35a69b8ac0ac1aff4e18209b845f4ba70eb79ba9
Michial Andrew Green II <vmichial@gmail.com>
2013-09-04 23:42:11 -0700

    working on Menu class
    buttons and Labels will be a part of it

--- Libraries/Classes/Game/Core/Menu.js
+++ Libraries/Classes/Game/Core/Menu.js
@@ -6,17 +6,47 @@
 	and iterate through them. at a set speed. it will randomly pan
 	through the images. I can play a sound if you wish for bgm.
 */
-var Button = function(Parent,img,Xdraw,Ydraw,Height,Width){
+var Label = function(Parent, Text, fontSize, IMG, TextX,TextY,ImX,ImY){
 	var that = this;
+	this.img = IMG;
+	this.text = Text;
+	this.textX = TextX;
+	this.textY = Texty;
+	this.imX = ImX;
+	this.imY = Imy;
+	this.font = fontSize+"px Arial";
+	
+	this.draw(){
+		if(that.text!='undefined'){
+			Parent.context.font = font;
+			Parent.context.fillText(Text,textX,textY);
+		}
+		if(that.img != 'undefined'){
+			Parent.context.drawImage(img,imX,imY);
+		}
+		if(that.text=='undefined' && that.img=='undefined'){
+			console.log("EHRMEGERD YOU PASSED NO IMAGE OR TEXT TO THE LABEL!");
+		}
+	}
+}
+
+var Button = function(Parent,img,Xdraw,Ydraw,Height,Width){
+	this.xdraw = Xdraw;
+	this.ydraw = Ydraw;
+	this.w = Width;
+	this.h = Height;
 	this.mommy = Parent;
 	this.ctx = mommy.context;
 	
+	this.clicked = function(x,y){
+		return (x>=xdraw && x<xdraw+this.w && y>ydraw && y<ydraw+this.h);
+	}
 	
 	this.draw = function(){
 		this.ctx.drawImage(img,Xdraw,Ydraw);
 	}
 }
-var Menu = function(Parent,BGimages,Music,Buttons){
+var Menu = function(Parent,BGimages,Music,Buttons,Labels){
 	this.context = Parent.context;
 	this.FPS = 60;
 	this.bgImages = BGimages;
