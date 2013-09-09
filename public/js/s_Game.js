d3c4294b3b88f329b77d248625fe4e2fa76da9d6
Edward Thang <RandomClown@gmail.com>
2013-09-09 05:03:48 -0700

    Sample: NPCs on World Grid

--- public/js/s_Game.js
+++ public/js/s_Game.js
@@ -11,21 +11,36 @@ function initGame() {
 		ent_cat.vel=1;
 
 		var tex_cat=new Texture('nyanCat', { x: 64, y: 64 });
-
 		ent_cat.addTex(tex_cat);
-		ent_cat.translate({ x: Math.random()*700+50, y: Math.random()*500+50 });
-		ent_cat.face(Math.random()*Math.PI*2);
-
 		s_game.addEnt(ent_cat);
+
+
+		// Remember the position in the grid based world
+		ent_cat.tilePos={
+			x: Math.round(Math.random()*s_world.size.x), // Random initial position
+			y: Math.round(Math.random()*s_world.size.y)
+		};
+		ent_cat.translate(s_world.getTilePos(ent_cat.tilePos)); // Convert to XY coord & translate there
+
+		ent_cat.face(Math.random()*Math.PI*2);
 
 
 		// Lets make the cats move randomly
 		ent_cat.dest={ x: ent_cat.pos.x, y: ent_cat.pos.y }; // Remember a destination
 		ent_cat.onStep=function() { // onStep() is 1 of the 2 customizable event
+			this.turn(this.dest);
 			var reached=this.move(this.dest);
 			if(reached) {
-				this.dest.x=this.pos.x+Math.random()*64-32; // Choose a random spot
-				this.dest.y=this.pos.y+Math.random()*64-32;
+				var dir=Math.round(Math.random()*4); // Random assign a direction
+				var newX=this.tilePos.x+(dir==0?1:dir==1?-1:0);
+				var newY=this.tilePos.y+(dir==2?1:dir==3?-1:0);
+
+				this.tilePos={ // Make sure it doesnt go off the world
+					x: (newX<0?0:(s_world.size.x<=newX?s_world.size.x-1:newX)),
+					y: (newY<0?0:(s_world.size.y<=newY?s_world.size.y-1:newY))
+				}; // This will be included in the library in the future
+
+				this.dest=s_world.getTilePos(this.tilePos)
 			}
 
 			// The default step, which steps all animations.
